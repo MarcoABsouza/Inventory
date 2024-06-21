1605d63b31d29488b9d41297b31f163ae3c8a2c5
marco <mabdsouza@outlook.com>
2024-06-21 10:41:16 -0300

    modifying database, attributes now have not null

--- db.py
+++ db.py
@@ -6,4 +6,4 @@ connection = lite.connect("inventory/database.db")
 # create table
 with connection:
     cursor = connection.cursor()
-    cursor.execute("CREATE TABLE if not exists Inventory(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, location TEXT, description TEXT, brand TEXT, date_of_purchase DATE, purchase_value DECIMAL)")
+    cursor.execute("CREATE TABLE if not exists Inventory(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name TEXT NOT NULL, location TEXT NOT NULL, description TEXT NOT NULL, brand TEXT NOT NULL, date_of_purchase DATE NOT NULL, purchase_value DECIMAL NOT NULL)")
